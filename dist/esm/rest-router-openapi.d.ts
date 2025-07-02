a9991ef8f827fdba14265479fec50c8388169b13
e22m4u <e22m4u@yandex.ru>
2025-07-03 01:56:56 +0300

    chore: disables container extension

--- dist/esm/rest-router-openapi.d.ts
+++ dist/esm/rest-router-openapi.d.ts
@@ -11,6 +11,10 @@ export declare const OPENAPI_VERSION = "3.1.0";
  */
 export declare class RestRouterOpenAPI extends Service {
     /**
+     * Constructor.
+     */
+    constructor();
+    /**
      * Добавляет параметр в операцию.
      *
      * @param oaOperation
--- dist/esm/rest-router-openapi.js
+++ dist/esm/rest-router-openapi.js
@@ -36,6 +36,14 @@ const DATA_TYPE_TO_OA_MEDIA_TYPE = new Map([
  */
 export class RestRouterOpenAPI extends Service {
     /**
+     * Constructor.
+     */
+    constructor() {
+        // запрет передачи контейнера в качестве первого
+        // аргумента данного сервиса
+        super();
+    }
+    /**
      * Добавляет параметр в операцию.
      *
      * @param oaOperation
